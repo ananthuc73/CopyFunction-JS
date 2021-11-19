cd8a82a649b96c7600612a03b6cddad7497005b8
Ananthu <43871170+ananthuc73@users.noreply.github.com>
2021-11-19 10:45:11 +0530

    Add files via upload

---
+++ copyfnctn.js
@@ -0,0 +1,6 @@
+function copy(){
+	var x=document.getElementById("a").value;
+	document.getElementById("c").value=x;
+	var x=document.getElementById("b").value;
+	document.getElementById("d").value=x;
+}
